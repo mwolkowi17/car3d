fc7c5a8305542a79cb5135448718407f3a769d3e
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-11-15 23:01:27 +0100

    version 0 ready

---
+++ src/loader.js
@@ -0,0 +1,18 @@
+import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader';
+import * as THREE from 'three';
+import {scene} from './index'
+
+const gltfLoader2 = new GLTFLoader()
+    gltfLoader2.load('./scene.gltf', (gltf) => {
+      const root = gltf.scene;
+      //const anim = gltf.animations;
+      root.position.x=-2;
+      root.rotation.y=Math.PI;
+      scene.add(root);
+      //mixer = new THREE.AnimationMixer(root);
+
+      //action = mixer.clipAction( anim[ 0 ] )
+      //action.play();
+
+     
+    })
