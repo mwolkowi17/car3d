6e092800bceb4eaff8be3ecb52a1e1d8309aecb0
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-11-16 21:27:12 +0100

    copywrights

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.shadowMap.enabled = true;
+
 document.body.appendChild(renderer.domElement);
 
 scene.background = new THREE.Color(0x000000);
