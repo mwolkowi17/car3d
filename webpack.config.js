0b507fe155609ef7f85a4f341280e037205bbf1e
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-11-21 12:36:29 +0100

    babel added

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,19 @@ module.exports = {
     },
     compress: true,
     port: 9000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 };
