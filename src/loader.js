26c3a0ff5defd351f573a25f738c82c42a6e6ef9
Marcin Wołkowicz <mwolkowicz17@gmail.com>
2021-11-21 20:50:56 +0100

    cleaning up

--- src/loader.js
+++ src/loader.js
@@ -11,14 +11,11 @@ import {scene} from './index'
 export const gltfLoader2 = new GLTFLoader()
     gltfLoader2.load('./scene.gltf', (gltf) => {
       const root = gltf.scene;
-      //const anim = gltf.animations;
+     
       root.position.x=-2;
       root.rotation.y=Math.PI;
       scene.add(root);
-      //mixer = new THREE.AnimationMixer(root);
-
-      //action = mixer.clipAction( anim[ 0 ] )
-      //action.play();
+      
       const progressInfo=document.getElementsByClassName('progressInfo')[0];
       progressInfo.style.visibility = 'hidden';
      
